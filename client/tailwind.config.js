976b527f18c5d339be70660cb38f91633b9b3d33
sam chappel <m.samchappel@gmail.com>
2023-09-17 20:52:25 -0700

    updated tailwindCSS configuration and post-build script for deployment

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false,
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: media,
   daisyui: {
     themes: [
       {
